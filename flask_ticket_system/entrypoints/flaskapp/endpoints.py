b2a9826f9fb522078e3a56811cde49013ef14b04
Dmytro Maior <mayor4090Gmail.com>
2024-07-24 23:20:03 +0300

    Update Flask app

--- flask_ticket_system/entrypoints/flaskapp/endpoints.py
+++ flask_ticket_system/entrypoints/flaskapp/endpoints.py
@@ -1,10 +1,29 @@
 from flask import Response, jsonify, request
 from pydantic import ValidationError
 
+from flask_ticket_system.domain.commands import CreateTicketCommand
+from flask_ticket_system.domain.tickets import Assigment, Ticket
+from flask_ticket_system.entrypoints.flaskapp.messagebus import message_bus
 from flask_ticket_system.entrypoints.flaskapp.middleware import model_middleware
+from flask_ticket_system.entrypoints.flaskapp.schemes import Assigment as PAssigment
 from flask_ticket_system.entrypoints.flaskapp.schemes import CreateTicket
 
 
 @model_middleware(CreateTicket)
 def create_ticket(create_ticket: CreateTicket):
-    return Response(str(create_ticket.model_dump()), status=200)
+    ticket: Ticket = message_bus.handle(
+        CreateTicketCommand(
+            create_ticket.title,
+            create_ticket.content,
+            Assigment(
+                create_ticket.assigment.assigment_type,
+                create_ticket.assigment.assigment_id,
+            ),
+        )
+    )
+
+    return jsonify(
+        {
+            "id": ticket.id,
+        }
+    )
--- flask_ticket_system/entrypoints/flaskapp/messagebus.py
+++ flask_ticket_system/entrypoints/flaskapp/messagebus.py
@@ -3,8 +3,9 @@ from typing import TYPE_CHECKING
 from kytool.factories import create_message_bus
 
 from flask_ticket_system.service_layer.handlers import *
+from flask_ticket_system.service_layer.unit_of_work import InMemoryUnitOfWorkPool
 
 if TYPE_CHECKING:
     from kytool.service_layer.messagebus import MessageBus
 
-message_bus: "MessageBus" = create_message_bus()
+message_bus: "MessageBus" = create_message_bus(uow_pool=InMemoryUnitOfWorkPool())
