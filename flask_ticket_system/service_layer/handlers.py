eeb4461fea8a4dbd8bfde53a8e8f7a924369f2dc
Dmytro Maior <mayor4090Gmail.com>
2024-07-24 23:19:57 +0300

    Add handlers

--- flask_ticket_system/service_layer/handlers.py
+++ flask_ticket_system/service_layer/handlers.py
@@ -1,8 +1,21 @@
+from kytool.adapters.repository import AbstractRepository
 from kytool.service_layer.handlers import register_handler
 
+from flask_ticket_system.domain import Ticket, TicketStatus, User
 from flask_ticket_system.domain.commands import CreateTicketCommand
+from flask_ticket_system.service_layer.unit_of_work import AbstractUnitOfWork
 
 
 @register_handler(CreateTicketCommand)
-def create_ticket_handler(command: CreateTicketCommand):
-    print("Create ticket handler")
+def create_ticket_handler(
+    command: CreateTicketCommand,
+    uow: AbstractUnitOfWork,
+) -> Ticket:
+    ticket = Ticket.create(
+        title=command.title,
+        content=command.content,
+        assigment=command.assignment,
+    )
+    uow.tickets.add(ticket)
+    uow.commit()
+    return ticket
